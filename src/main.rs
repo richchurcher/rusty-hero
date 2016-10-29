7bf054bb0478b958c0b56318ad92067628e40401
Rich Churcher <rich.churcher@gmail.com>
2016-10-29 19:07:29 +1300

    Do a rough texture update

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,22 @@ extern crate sdl2;
 
 use sdl2::event::{Event, WindowEventId};
 use sdl2::keyboard::Keycode;
-use sdl2::render::{Renderer, TextureAccess};
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::render::Renderer;
 
 fn update_window(renderer: &mut Renderer) {
     let viewport = renderer.viewport();
-    let texture = renderer.create_texture(sdl2::pixels::PixelFormatEnum::ARGB8888, TextureAccess::Streaming, viewport.width(), viewport.height());
+    let width = viewport.width();
+    let height = viewport.height();
+    let pitch = width * 4;
+    let bytes = pitch * height;
+    let pixels = vec![0; bytes as usize];
+
+    let mut texture = renderer.create_texture_streaming(
+        PixelFormatEnum::ARGB8888,
+        width, height).unwrap();
+    texture.update(None, &pixels[..], pitch as usize).unwrap();
+
     renderer.clear();
     renderer.present();
 }
